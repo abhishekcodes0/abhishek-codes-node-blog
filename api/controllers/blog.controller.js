8d0a53c4a763d7ae01fe9c6caa776a2122609cfe
kaushal jalal <abhisheksingh2239@gmail.com>
2024-06-18 16:38:43 +0530

    image upload functionality added

--- api/controllers/blog.controller.js
+++ api/controllers/blog.controller.js
@@ -1,6 +1,11 @@
 import Blog from "../models/blog.model.js";
 import { errorHandler } from "../utils/error.js";
 
+import { s3 } from "../aws-config.js";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 } from "uuid";
+import path from "path";
+
 export const createBlog = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not authorised to do this"));
@@ -77,6 +82,27 @@ export const updateBlog = async (req, res, next) => {
       }
     );
     res.status(200).json(updatedBlog._doc);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const uploadImage = async (req, res, next) => {
+  const file = req.file;
+  const fileName = `${v4()}${path.extname(file.originalname)}`;
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: fileName,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+    ACL: "public-read", // Make the file publicly readable
+  };
+
+  try {
+    await s3.send(new PutObjectCommand(params));
+    const url = `https://${params.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
+    res.status(200).send({ url });
   } catch (error) {
     next(error);
   }
--- api/routes/blog.route.js
+++ api/routes/blog.route.js
@@ -5,8 +5,13 @@ import {
   getSingleBlog,
   getPublishedBlogs,
   updateBlog,
+  uploadImage,
 } from "../controllers/blog.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+
+import multer from "multer";
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
@@ -15,5 +20,6 @@ router.get("/get", getPublishedBlogs);
 router.get("/get/all", getBlogs);
 router.get("/get/:slug", getSingleBlog);
 router.put("/update/:slug", verifyToken, updateBlog);
+router.post("/upload-image", verifyToken, upload.single("image"), uploadImage);
 
 export default router;
