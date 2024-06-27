512b6c0e7ed6203bdf8e417e746220cecd7893d2
kaushal jalal <abhisheksingh2239@gmail.com>
2024-06-27 19:53:52 +0530

    secret keys issue

--- api/aws-config.js
+++ api/aws-config.js
@@ -1,20 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { secret } from "./app.js";
 
-export const s3 = new S3Client(
-  process.env.NODE_ENV == "production"
-    ? {
-        region: secret?.AWS_REGION,
-        credentials: {
-          accessKeyId: secret?.AWS_ACCESS_KEY_ID,
-          secretAccessKey: secret?.AWS_SECRET_ACCESS_KEY,
-        },
-      }
-    : {
-        region: process.env.AWS_REGION,
-        credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        },
-      }
-);
+export const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
